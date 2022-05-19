62d36f69282c19d957af3f15593be1cab25cea28
AkarExcel <therealakarexcel@gmail>
2022-05-19 17:03:17 +0100

    add the image to the Meme component and made the form reactive

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,30 +3,67 @@ import memesData from '../memesData'
 
 const Meme = () => {
 
-    function handleOnClick (e){
-        const memeArray = memesData.data.memes;
-        const randomNumber = (Math.floor(Math.random() * memeArray.length))
-        console.log(randomNumber)
-        e.preventDefault();
+    const [meme, setMeme] = React.useState(
+        {
+            topText: "",
+            bottomText: "",
+            memeImage: ""
+        }
+    )
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(meme.memeImage)
+
+    }
+    
+    function handleOnChange (e){
+        const {name, value} = e.target
+        setMeme(prevState => ({
+            
+            ...prevState,
+            [name] : [value]
+
+        }))
+    }
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            memeImage: url
+        }))
     }
 
   return (
     <main>
-        <form className='form'>
+        <form className='form' onSubmit={handleSubmit}>
             <input 
                 type='text'
                 placeholder='Top text'
                 className='form--input'
+                name="topText"
+                value={meme.topText}
+                onChange={handleOnChange}
             />
             <input 
                 type='text'
                 placeholder='Bottom Text'
                 className='form--input'
+                name="bottomText"
+                value={meme.bottomText}
+                onChange={handleOnChange}
             />
-            <button onClick={handleOnClick} className='form--button'>
+            <button onClick={getMemeImage} className='form--button'>
                 Get me a meme image 🐸
             </button>
         </form>
+        {
+            meme.memeImage ? <div className="meme"> <img src={meme.memeImage} className="meme--image" alt='meme ' /> <h2 className="meme--text top">{meme.topText}</h2> <h2 className="meme--text bottom">{meme.bottomText}</h2> </div> : console.log("No Image")
+        }
+        
     </main>
   )
 }
